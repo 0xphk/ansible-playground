2bc09b6f620ddc16252c0bcbe943d2947f11f2b2
phk <0xphk@terra.com>
2016-09-01 11:55:13 +0200

    fixed date

--- resources/sel/check_sel.sh
+++ resources/sel/check_sel.sh
@@ -9,7 +9,7 @@ HOSTNAME=$(hostname -f)
 if ipmitool sel | grep true;
   then
     echo "System event log on $HOSTNAME needs rotation: $(ipmitool sel)" | mail -s "[WARNING] $HOSTNAME system event log full" root;
-    ipmitool sel elist | mail -s "[INFO] $HOSTNAME system event log dump $(date +%Y%m%d" root;
+    ipmitool sel elist | mail -s "[INFO] $HOSTNAME system event log dump $(date +%Y%m%d)" root;
     ipmitool sel clear && sleep 5;
     echo "System event log on $HOSTNAME: $(ipmitool sel | grep Percent)" | mail -s "[INFO] $HOSTNAME system event log status" root;
   else
